ce67e5887f0b264973e70a41c4a749263443ce0f
sufyangit22 <mmuhammadsufyan5@gmail.com>
2021-06-14 02:15:57 -0700

    updated footer and portfolio section

--- assets/js/swiperSlider.js
+++ assets/js/swiperSlider.js
@@ -4,24 +4,21 @@ const swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination',
         clickable: true,
     },
-    // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next-unique',
+        prevEl: '.swiper-button-prev-unique'
     },
-
     breakpoints: {
         // when window width is >= 320px
-        270: {
+        0: {
             slidesPerView: 1,
             spaceBetween: 0,
         },
         // when window width is >= 480px
-        480: {
+        500: {
             slidesPerView: 2,
             spaceBetween: 10,
         },
-        // when window width is >= 640px
         786: {
             slidesPerView: 3,
             spaceBetween: 20,
@@ -29,12 +26,3 @@ const swiper = new Swiper('.swiper-container', {
     }
 
 });
-
-// var swiper = new Swiper('.swiper-container', {
-//     slidesPerView: 3,
-//     spaceBetween: 30,
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//     },
-// });
