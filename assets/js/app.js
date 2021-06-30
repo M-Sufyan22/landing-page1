380156038c99368b8cab608c348b10ac0bf1324d
sufyangit22 <mmuhammadsufyan5@gmail.com>
2021-06-30 04:07:05 -0700

    change made

--- assets/js/app.js
+++ assets/js/app.js
@@ -114,7 +114,7 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-
+    let countDiv;
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("demo");
@@ -122,10 +122,19 @@ function showSlides(n) {
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
+        slides[i].classList.remove("active-boxl");
+
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace("active", "");
+
     }
     slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].classList.add("active-boxl");
     dots[slideIndex - 1].className += "active";
+    countDiv = document.querySelector('.active-boxl .numbertext');
+    if (countDiv) {
+        countDiv.innerHTML = `${n  > slides.length ? 1 : n }/${slides.length}`
+    }
+    console.log(slides.length, n - 1 > slides.length ? slides.length : n - 1)
 }
